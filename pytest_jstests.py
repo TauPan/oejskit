b0b285cb92d2c5f087b1490db483043f34fbdda5
Samuele Pedroni <pedronis@lucediurna.net>
2009-06-16 17:00:10 +0200

    collect js files starting with jstest_, renamings in the code

--- pytest_jstests.py
+++ pytest_jstests.py
@@ -50,73 +50,89 @@ def pytest_pycollect_makeitem(collector, name, obj):
 
 class RunState:
 
-    def __init__(self, modcol):
-        self.modcol = modcol
+    def __init__(self, collector):
+        self.collector = collector
 
     def getglobal(self, name):
         try:
-            return self.modcol.config.getvalue(name)
+            return self.collector.config.getvalue(name)
         except KeyError:
             raise AttributeError(name)
 
     def getscoped(self, name):
-        pluginmanager = self.modcol.config.pluginmanager
+        pluginmanager = self.collector.config.pluginmanager
         plugins = pluginmanager.getplugins()
-        plugins.append(self.modcol.obj)
+        if hasattr(self.collector, 'obj'):
+            plugins.append(self.collector.obj)
         return pluginmanager.listattr(attrname=name, plugins=plugins)[-1]
 
     @property
     def testdir(self):
-        return os.path.dirname(self.modcol.obj.__file__)
+        if hasattr(self.collector, 'obj'):
+            return os.path.dirname(self.collector.obj.__file__)
+        else:
+            return str(self.collector.fspath.dirpath())
 
     @property
     def testname(self):
-        return self.modcol.obj.__name__
+        if hasattr(self.collector, 'obj'):        
+            return self.collector.obj.__name__
+        else:
+            return self.collector.name
         
 
 _run = {}
 
 def get_state(item, collect=False):
-    modcol = item.getparent(py.test.collect.Module)
+    collector = item.getparent((py.test.collect.Module, JsFile))
     try:
-        return _run[modcol]
+        return _run[collector]
     except KeyError:
         pass
-    _run[modcol] = state = RunState(modcol)
+    _run[collector] = state = RunState(collector)
     if not collect:
         #print "ADD FINALIZER", os.getpid()
         #import traceback; traceback.print_stack()        
-        modcol.config._setupstate.addfinalizer(colitem=modcol,
-                                     finalizer=lambda: del_state(modcol))
+        collector.config._setupstate.addfinalizer(colitem=collector,
+                                     finalizer=lambda: del_state(collector))
     return state
 
 def del_state(item):
-    modcol = item.getparent(py.test.collect.Module)    
-    state = _run.pop(modcol, None)
+    collector = item.getparent((py.test.collect.Module, JsFile))    
+    state = _run.pop(collector, None)
     if state:
         from oejskit.testing import cleanupBrowsers
         cleanupBrowsers(state)        
 
 def pytest_collectstart(collector):
-    if isinstance(collector, py.test.collect.Module):
+    if isinstance(collector, (py.test.collect.Module, JsFile)):
         get_state(collector, collect=True)
 
 def pytest_collectreport(rep):
     collector = rep.collector
-    if isinstance(collector, py.test.collect.Module):
+    if isinstance(collector, (py.test.collect.Module, JsFile)):
         del_state(collector)
 
 def pytest_unconfigure(config):
     for colitem in _run.keys():
         del_state(colitem)
 
+# jstest_*.js collection
+
+def pytest_collect_file(path, parent):
+    basename = path.basename
+    if basename.startswith('jstest_') and basename.endswith('.js'):
+        return JsFile(path, parent)
+    return None
+
 # ________________________________________________________________
 # items
 
-def give_browser(clsitem, attach=True):
+def give_browser(item, attach=True):
     from oejskit.testing import giveBrowser
-    return giveBrowser(get_state(clsitem), clsitem.obj, clsitem.browserKind,
-                                                        attach=attach)
+    return giveBrowser(get_state(item), getattr(item, 'obj', None),
+                                        item.browserKind,
+                                        attach=attach)
 
 def detach_browser(clsitem):
     from oejskit.testing import detachBrowser
@@ -249,5 +265,59 @@ class JsTest(py.test.collect.Function):
         fspath, lineno = self.parent._getfslineno()
         return fspath, lineno, self.getmodpath()
 
+# js files
+
+# XXX share more code
+
+class JsFile(py.test.collect.File):
+
+    def __init__(self, fspath, parent):
+        super(JsFile, self).__init__(fspath, parent)
+        self.browserKind = fspath.purebasename.split('_')[-1]
+
+    def collect(self):
+        l = []
+        kinds = expand_browsers(self.config, self.browserKind)
+        if not kinds:
+            py.test.skip("no browser of kind %s" % self.browserKind)
+        for kind in kinds:
+            name = "[=%s]" % kind
+            suite = JsFileSuite(name, self, kind)
+            l.append(suite)
+        return l
 
 
+class JsFileSuite(py.test.collect.Collector):
+
+    def __init__(self, name, parent, browserKind):
+        super(JsFileSuite, self).__init__(name, parent)
+        self.browserKind = browserKind
+
+    # accomodate JsTest/Function assumption
+    obj = None
+    
+    def _getfslineno(self):
+        return self.parent.fspath, 0
+    # 
+
+    def setup(self):
+        browser, setupBag = give_browser(self, attach=False)
+        super(JsFileSuite, self).setup()
+
+    # nothing special to do in teardown
+
+    def collect(self):
+        browser, setupBag = give_browser(self, attach=False)
+        url = self.parent.fspath.basename
+        names, runner = browser._gatherTests(url, setupBag)
+
+        def runTest(jstest):
+            runner._runTest(jstest, None, None)
+            
+        l = []
+        for jstest in names:
+            name = "[%s]" % jstest
+            function = JsTest(name=name, parent=self, 
+                              args=(jstest,), callobj=runTest)
+            l.append(function)
+        return l
