ceb0d0fed82b3fb523571f147fcd50e6c61d49af
Samuele Pedroni <pedronis@lucediurna.net>
2009-06-16 14:39:07 +0200

    don't reuse / from various tests, IE will cache

--- test/test_integration_style.js
+++ test/test_integration_style.js
@@ -2,7 +2,7 @@
 
 Tests = {
     test_get_ok: function() {
-        var ok = OpenEnd._GET('/')
+        var ok = OpenEnd._GET('/integration') // naive work around IE caching issues
         ais(ok, "ok\n")
     }
 }
