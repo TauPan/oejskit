78e0f00d2e2403331c64efc2963187faa916977a
Samuele Pedroni <pedronis@lucediurna.net>
2009-08-20 08:54:02 +0200

    test fix for py.lib 1.0.1

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,6 @@
 pytest_plugins = "jstests"
 
-pytest_option_jstests_reuse_browser_windows = True
+option_jstests_reuse_browser_windows = True
 
 jstests_browser_specs = {
     'supported': ['firefox', 'iexplore'],
