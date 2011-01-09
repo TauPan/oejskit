088def38e16e46526617e408577989012f83af8a
Samuele Pedroni <pedronis@lucediurna.net>
2011-01-09 14:24:49 +0100

    fixes, pep8 style

--- setup.py
+++ setup.py
@@ -8,7 +8,22 @@
 from setuptools import setup
 import os, sys
 
+
 version = '0.9.0'
+
+BRIDGE = """
+`rest of the docs... <http://lucediurna.net/oejskit/doc/doc.html#rest-of-the-docs>`_
+
+`Europython 2009 talk with examples <http://lucediurna.net/oejskit/talk>`_
+
+The project repository lives at http://bitbucket.org/pedronis/js-infrastructure/
+
+Discussions and feedback should go to py-dev at codespeak.net
+
+Changelog
+-----------
+"""
+
 
 def long_descr():
     lines = open('doc/doc.txt', 'r').readlines()
@@ -17,14 +32,11 @@ def long_descr():
         if 'rest of the docs' in line:
             break
         start.append(line)
-    start.append("`rest of the docs... <http://lucediurna.net/oejskit/doc/doc.html#rest-of-the-docs>`_\n\n")
-    start.append("`Europython 2009 talk with examples <http://lucediurna.net/oejskit/talk>`_\n\n")
-    start.append("The project repository lives at http://bitbucket.org/pedronis/js-infrastructure/\n\n")
-    start.append("Discussions and feedback should go to py-dev at codespeak.net\n")
-    start.append("\nChangelog\n-----------\n")
+    start.append(BRIDGE.lstrip())
     start.append(open('CHANGELOG.txt', 'r').read())
     descr = ''.join(start)
     return descr
+
 
 setup(
     name="oejskit",
@@ -33,7 +45,8 @@ setup(
     long_description=long_descr(),
     license='MIT',
     author='Open End AB',
-    author_email='py-dev@codespeak.net,pedronis@openend.se', # with hpk approval at ep09
+    # with hpk approval at ep09
+    author_email='py-dev@codespeak.net,pedronis@openend.se',
     url='http://bitbucket.org/pedronis/js-infrastructure/',
     platforms=['linux', 'osx', 'win32'],
     packages=['oejskit'],
@@ -52,7 +65,7 @@ setup(
     'Programming Language :: Python',
     'Programming Language :: JavaScript'
     ],
-    entry_points = {
+    entry_points={
     'pytest11': ['pytest_jstests = oejskit.pytest_jstests'],
     }
 )
